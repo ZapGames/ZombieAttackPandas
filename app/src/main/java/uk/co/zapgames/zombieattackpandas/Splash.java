d2bf1b29130ef540cbf89f8aab03f83910ce87c8
Scott Robinson <scottwilliamrobinson@gmail.com>
2014-12-26 19:23:40 +0000

    making splash

--- app/src/main/java/uk/co/zapgames/zombieattackpandas/Splash.java
+++ app/src/main/java/uk/co/zapgames/zombieattackpandas/Splash.java
@@ -4,14 +4,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Splash extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+
+        final Animation alphaIn = AnimationUtils.loadAnimation(this, R.anim.alpha_in);
+        alphaIn.reset();
+        final ImageView splashPic = (ImageView) findViewById(R.id.splashView);
+        splashPic.startAnimation(alphaIn);
+
+
     }
 
 
